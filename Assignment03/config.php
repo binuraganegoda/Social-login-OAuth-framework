24c61401a291097b4393d19a9a5c7bdcc229c3be
binuraganegoda <asviganegoda@gmail.com>
2018-05-09 21:11:47 +0530

    change
    change

--- Assignment03/config.php
+++ Assignment03/config.php
@@ -7,6 +7,6 @@
     $gClient->setClientId("798525245171-m00nbrf6j9gj9pt1ru39l9a1n0ing1fd.apps.googleusercontent.com");
     $gClient->setClientSecret("pAalE9YZ_x69Krqa86SbSIbe");
     $gClient->setApplicationName("SSS Social Login");
-    $gClient->setRedirectUri("http://localhost/Google/g-callback.php");
+    $gClient->setRedirectUri("http://localhost/Assignment03/g-callback.php");
     $gClient->addScope("https://www.googleapis.com/auth/plus.login https://www.googleapis.com/auth/userinfo.email");
 ?>
--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # Social-login-OAuth-framework
+
+This example shows how to implement Social-login-OAuth-framework
+
+For More details visit : https://whitehatuser.wordpress.com/2018/05/09/social-login-with-oauth-2-0/
+
+Default Username : 
+
+Default Password : 123
+
+Desgin and Developed by : Nisal Priyanka
+Mail : nisalpriyanka96@gmail.com
